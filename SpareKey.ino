3aa4c73ba2585fe33a8396d264622f0cd627bbe3
david.klempfner <dklempfner@gmail.com>
2020-05-16 17:12:50 +1000

    Put back working version for ESP32.

--- SpareKey.ino
+++ SpareKey.ino
@@ -1,27 +1,18 @@
 //Pin Reference: https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
 //Tone library: https://github.com/lbernstone/Tone
 #include <WiFi.h>
-#include <WiFiUdp.h>
-#include <NTPClient.h>
 #include <Tone32.h>
 
 const size_t input_buffer_length = 256;
 
 const char* ssid = "iiNetC2DAD7";
-const char* wifiPassword = "M9PrxcbtkS";
+const char* wifiPassword = "aM9PrxcbtkS";
 const char* passwordToOpenDoor = "/87"; //password should begin with a slash
 
 const int doorPin = 17;
 const int buzzerPin = 16;
 const int toneDuration = 700;
 const int buzzerChannel = 0;
-
-const int lockedOutTimeInSeconds = 15;
-const int maxBadPasswordCount = 3;
-int badPasswordCount = 0;
-long timeWhenUserCanTryNewPassword = 0;
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 0);
 
 const int LED_BUILTIN = 2;
 
@@ -32,7 +23,7 @@ WiFiServer server(301); //Pick any port number you like
 void setup() {
   Serial.begin(115200);
   delay(10);
-  Serial.println(WiFi.localIP()); 
+  Serial.println(WiFi.localIP());
   
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, 0);
@@ -58,8 +49,6 @@ void setup() {
   server.begin();
   Serial.println("Server started.");
   Serial.println(WiFi.localIP());  
-
-  timeClient.begin();
 }
 
 void loop() {      
@@ -76,50 +65,17 @@ void loop() {
     char request[input_buffer_length];
     client.readBytesUntil('\r', request, input_buffer_length);  
     Serial.println(request);
-
-    long currentEpochTime = GetCurrentEpochTime();
-    bool doesUserStillHaveToWait = currentEpochTime < timeWhenUserCanTryNewPassword;
-    if (doesUserStillHaveToWait){
-      long remainingLockOutTime = timeWhenUserCanTryNewPassword - currentEpochTime;     
-      char remainingLockOutTimeStrBuffer[16] = {'\0'};
-      itoa(remainingLockOutTime, remainingLockOutTimeStrBuffer, 10);
-      char stillHavetoWaitMsg[input_buffer_length];
-      strcat(stillHavetoWaitMsg, "Please wait ");
-      strcat(stillHavetoWaitMsg, remainingLockOutTimeStrBuffer);
-      strcat(stillHavetoWaitMsg, " seconds");      
-      GenerateResponse(client, stillHavetoWaitMsg);
-    }
-    else if(strstr(request, passwordToOpenDoor)) {
+    
+    if(strstr(request, passwordToOpenDoor)) {
       GenerateResponse(client, "Password is correct.");
       OpenDoor();
       CorrectPasswordSound();
-      badPasswordCount = 0;
     }  
     else if (!strstr(request, "favicon.ico")) {
       //Got a GET request and it wasn't the favicon.ico request, must have been a bad password:
-      badPasswordCount++;
-      if (badPasswordCount < maxBadPasswordCount) {
-          GenerateResponse(client, "Password is incorrect.");
-      }
-      else { //Disrupt brute-force attacks:
-        char lockedOutTimeInSecondsStrBuffer[16] = {'\0'};
-        itoa(lockedOutTimeInSeconds, lockedOutTimeInSecondsStrBuffer, 10);
-        char badPasswordAndStillHavetoWaitMsg[input_buffer_length] = {'\0'};
-        strcat(badPasswordAndStillHavetoWaitMsg, "Password is incorrect. Please wait ");
-        strcat(badPasswordAndStillHavetoWaitMsg, lockedOutTimeInSecondsStrBuffer);
-        strcat(badPasswordAndStillHavetoWaitMsg, " seconds");      
-        GenerateResponse(client, badPasswordAndStillHavetoWaitMsg);        
-        timeWhenUserCanTryNewPassword = GetCurrentEpochTime() + lockedOutTimeInSeconds;
-        badPasswordCount = 0;
-        TooManyIncorrectPasswordsSound();
-      }
-    }
+      GenerateResponse(client, "Password is incorrect.");
+    }  
   }
-}
-
-long GetCurrentEpochTime(){
-  timeClient.update();
-  return timeClient.getEpochTime();
 }
 
 void OpenDoor() {
@@ -154,11 +110,4 @@ void CorrectPasswordSound() {
     tone(buzzerPin, frequency, toneDuration, buzzerChannel);
     noTone(buzzerPin, buzzerChannel);
   }
-}
-
-void TooManyIncorrectPasswordsSound() {  
-  int frequency = 400;    
-  int badPasswordToneDuration = 3 * toneDuration;
-  tone(buzzerPin, frequency, badPasswordToneDuration, buzzerChannel);  
-  noTone(buzzerPin, buzzerChannel);  
 }
